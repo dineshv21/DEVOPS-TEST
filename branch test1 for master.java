ae1f8cd9887d441461c47a2c8c7cd5d1ba5b5ad3
dineshv21 <dineshece.21@gmail.com>
2020-07-05 12:20:35 +0530

    Revert "test"
    This reverts commit e7a9663d2c89cf8387ae27942dbabc3bdc1c2896.

--- branch test1 for master.java
+++ branch test1 for master.java
@@ -3,6 +3,5 @@ class Hello
 public static void main (String args[])
 {
 System.out.println("Java Hello World-modified with branch test1 for master");
-System.out.println("Java Hello World-modified with branch test1 for master");
 }
 }
