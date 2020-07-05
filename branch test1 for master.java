359bdca020ebf98f845f1da76c8d8e5017bc8722
dineshv21 <dineshece.21@gmail.com>
2020-07-05 12:11:57 +0530

    Revert "Revert "rever check add""
    This reverts commit 50bea26a4230782fcf9f4679c14b1f04724aa391.

--- branch test1 for master.java
+++ branch test1 for master.java
@@ -3,5 +3,6 @@ class Hello
 public static void main (String args[])
 {
 System.out.println("Java Hello World-modified with branch test1 for master");
+System.out.println("Should be removed while doing revert");
 }
 }
